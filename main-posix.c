00b139f9b5ff696a6b41942ebbe5985cf437fe44
Lotte Steenbrink <lotte@zombietetris.de>
2015-02-04 16:33:43 +0100

    improve client

--- main-posix.c
+++ main-posix.c
@@ -8,6 +8,9 @@
 #include "debug.h"
 
 #define PORT 5683
+#define BUFSZ 4096
+
+int make_homemade_request(uint8_t* snd_buf);
 
 int main(int argc, char **argv)
 {
@@ -15,8 +18,8 @@ int main(int argc, char **argv)
 
     int fd;
     struct sockaddr_in servaddr, cliaddr;
-    uint8_t buf[4096];
-    uint8_t scratch_raw[4096];
+    uint8_t buf[BUFSZ];
+    uint8_t scratch_raw[BUFSZ];
     coap_rw_buffer_t scratch_buf = {scratch_raw, sizeof(scratch_raw)};
 
     fd = socket(AF_INET,SOCK_DGRAM,0);
@@ -64,8 +67,62 @@ int main(int argc, char **argv)
                 coap_dumpPacket(&rsppkt);
 #endif
                 sendto(fd, buf, rsplen, 0, (struct sockaddr *)&cliaddr, sizeof(cliaddr));
+
+                printf("[main-posix] Following up with own initial GET:\n");
+                
+                // clear buffer
+                memset(buf, 0, BUFSZ);
+
+                if (0 == make_homemade_request(buf)) {
+                    sendto(fd, buf, rsplen, 0, (struct sockaddr *)&cliaddr, sizeof(cliaddr));
+                    printf("[main-posix] homemade inital GET sent.\n");
+                }
             }
         }
     }
 }
 
+/* organic, local, gluten-free */
+int make_homemade_request(uint8_t* snd_buf)
+{
+    printf("creating example request...\n");
+    static char* msg= "hello";
+    size_t req_pkt_sz;
+    int errcode;
+
+    // cobble together CoAP packet
+    coap_header_t req_hdr = {
+        .ver = 1,
+        .t = COAP_TYPE_NONCON,
+        .tkl = 0,                  /* microcoap can't generate tokens anyway */
+        .code = COAP_RSPCODE_GET,  /* Not sure if this is correct... */
+        .id = {22,22}              /*let's see if this works :D */
+    };
+
+    coap_buffer_t payload = {
+        .p = (const uint8_t *) msg,
+        .len = strlen(msg)
+    };
+
+    coap_packet_t req_pkt = {
+        .hdr = req_hdr,
+        .tok = (coap_buffer_t) {}, /* No token */
+        .numopts = 0,
+        .opts = {},
+        .payload = payload
+    };
+
+    req_pkt_sz = sizeof(req_pkt);
+
+#ifdef DEBUG
+    printf("[main-posix] content:\n");
+    coap_dumpPacket(&req_pkt);
+#endif
+
+    // try to  write packet to send buffer
+    if (0 != (errcode = coap_build(snd_buf, &req_pkt_sz, &req_pkt))) {
+        printf("Error building packet! Error code: %i\nAborting. \n", errcode);
+        return 1;
+    }
+    return 0;
+}
